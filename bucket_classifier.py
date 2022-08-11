bd0a74524cc5f00ee131abe284f6f88c9d3cf6ed
peter-splitice <70050337+peter-splitice@users.noreply.github.com>
2022-08-11 13:54:14 -0700

    Updated Regressor.py function to version 1.3

--- bucket_classifier.py
+++ bucket_classifier.py
@@ -7,21 +7,21 @@ import os
 import csv
 
 # Preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 # Models
 from sklearn.linear_model import Lasso
 from sklearn.svm import SVR, SVC
-import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 # Dimensionality Reduction
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SequentialFeatureSelector
 
-
 # Metrics
-from sklearn.metrics import matthews_corrcoef, mean_squared_error, accuracy_score
+from sklearn.metrics import matthews_corrcoef, mean_squared_error, accuracy_score, make_scorer
 
 # Model Persistence
 from joblib import dump, load
@@ -236,31 +236,34 @@ def fs_regressor(x, y):
     return x, sfs
 
 ## Forward selection for our classifier.
-def fs_classifier(x, y):
+def fs_classifier(x, y, model):
     """
     Perform Sequential Forward Selection on the given dataset, but for 
-        the classifer portion of the model.  The model used is SVM w/RBF Kernel.
+        the classifer portion of the model.  MCC is the scorer used.
 
     Parameters
     ----------
     x: Input values of the dataset.
 
-    bucket: True = large bucket, False = small bucket.
+    y: Output values for the different classes of the dataset.
+
+    model: Model function used for Sequential Feature Selection.
 
     Returns
     -------
     x: Input values of the dataset with half of the features selected.
 
+    sfs: The SequentialFeatureSelector model
+
     """
 
     # Fit a feature selector to SVM w/RBF kernel classifier and use the 'accuracy' score.
     logger.debug('Forward Selection Starting')
-    clf = SVC(kernel='rbf')
-    sfs = SequentialFeatureSelector(clf, n_jobs=-1, scoring='accuracy')
+    sfs = SequentialFeatureSelector(model, n_jobs=-1, scoring=make_scorer(matthews_corrcoef))
     sfs.fit(x, y)
     x = sfs.transform(x)
     logger.debug('Forward Selection Finished')
-    
+
     return x, sfs
 
 
@@ -288,13 +291,61 @@ def principal_component_analysis(x):
 
     return x, pca
 
-def classifer_trainer(x, y):
+def hyperparameter_optimizer(x, y, params, model=SVC()):
+    """
+    Perform GridSearchCV to find and return the best hyperparmeters.  I'll use MCC score here.
+    
+    Parameters
+    ----------
+    x: Input values to perform GridSearchCV with.
+
+    y: Output values to create GridSearchCV with.
+
+    params: Dictionary of parameters to run GridSearchCV on.
+
+    model: The model that we are using for GridSearchCV
+
+    Returns
+    -------
+    bestvals: Optimzied hyperparameters for the model that we are running the search on.
+    
+    """
+
+    # Use GridsearchCV to get the optimized parameters.
+    logger.debug('GridSearchCV Starting')
+    clf = GridSearchCV(model, params, scoring=make_scorer(matthews_corrcoef), cv=5, n_jobs=-1)
+    clf.fit(x,y)
+
+    # Showing the best paramets found on the development set.
+    logger.debug('Best parameters set found on development set:')
+    logger.debug('')
+    logger.debug(clf.best_params_)
+    logger.debug('')
+
+    # Testing on the development set.
+    logger.debug('Grid scores on development set:')
+    logger.debug('')
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        logger.debug('%0.3f (+/-%0.03f) for %r' % (mean, std*2, params))
+    logger.debug('GridSearchCV Finished')
+    logger.debug('')
+
+    # Save the best parameters.
+    bestvals = clf.best_params_
+
+    return bestvals
+
+
+def classifer_trainer(x, y, params, model=SVC()):
     """
     Perform fitting on the reduced datasets and then make predictions.  The output values are in the log file.
 
     Parameters
     ----------
     x: Reduced set of input values.
+
     y: Output KI values that we are using for the training and validation sets.
 
     Returns
@@ -312,22 +363,25 @@ def classifer_trainer(x, y):
     valid_accuracy_sum = 0
     valid_mcc_sum = 0
 
+    optimized_features = hyperparameter_optimizer(x, y, params, model)
+
+    model.set_params(**optimized_features)
+
     for seed in seeds:
         i += 1
         logger.debug('Training:')
         x_train, x_valid, y_train, y_valid = train_test_split(x, y, test_size=0.2, random_state=seed)
-        clf = SVC(kernel='rbf')
-        clf.fit(x_train, y_train)
+        model.fit(x_train, y_train)
 
         logger.debug('Training Finished.')
 
         # Test the model on the training set.
-        y_train_pred = clf.predict(x_train)
+        y_train_pred = model.predict(x_train)
         train_accuracy = accuracy_score(y_train, y_train_pred)
         train_mcc = matthews_corrcoef(y_train, y_train_pred)
 
         # Test the model on the validation set.
-        y_valid_pred = clf.predict(x_valid)
+        y_valid_pred = model.predict(x_valid)
         valid_accuracy = accuracy_score(y_valid, y_valid_pred)
         valid_mcc = matthews_corrcoef(y_valid, y_valid_pred)
 
@@ -352,6 +406,22 @@ def classifer_trainer(x, y):
     logger.info('AVG Training Accuracy: %3.3f, AVG Training MCC: %3.3f, AVG Validation Accuracy: %3.3f, '
                 'AVG Validation MCC: %3.3f' %(train_accuracy_avg, train_mcc_avg, valid_accuracy_avg, valid_mcc_avg))
 
+def classifier_pipeline(x, y, model, params):
+    """
+    This function is our pipeline for the bucket classifier.  The outputs are recorded into a log file.
+    
+    Parameters
+    ----------
+    x: Input variables
+    
+    y: Output classes
+    
+    model: Model that we are using
+    
+    """
+    x, _ = fs_classifier(x, y, model)
+    x, _ = principal_component_analysis(x)
+    classifer_trainer(x, y, params, model)
 
 
 # Function to separate items into buckets.
@@ -373,33 +443,44 @@ def bucket_seperator(threshold):
     # Import the data.
     df, _ = import_data()
 
-    # Creates a column in our dataframe to classify into a "small" and "large" bucket based on a threshold.
-    df['Bucket'] = df['KI (nM)'] > threshold
+    # Creates a column in our dataframe to classify into 3 separate buckets.  A 'small' and 'large' bucket
+    # based on the threshold, and a 'do not measure bucket' for anything with a KI value of > 4000
+    df['Bucket'] = pd.cut(x=df['KI (nM)'], bins=(0, threshold, 4000, float('inf')), labels=(0,1,2))
 
     # Try basing the threshold off of log transform?
 
-    large_bucket_count = df[df['Bucket'] == True]['Name'].count()
-    small_bucket_count = df[df['Bucket'] == False]['Name'].count()
+    large_bucket_count = df[df['Bucket'] == 1]['Name'].count()
+    small_bucket_count = df[df['Bucket'] == 0]['Name'].count()
+    extra_bucket_count = df[df['Bucket'] == 2]['Name'].count()
 
     # If either bucket is less than a third of the total nubmer of samples, I need to throw an exception.
-    cutoff_length = int(len(df['Bucket'])/3)
+    cutoff_length = int(len(df['Bucket'])/4)
 
     # The threshold was too large.
     if large_bucket_count < cutoff_length or small_bucket_count < cutoff_length:
-        logger.error('Threshold of %3.3f was too large. Large Bucket Size: %i, Small Bucket Size: %i' 
-                     %(threshold, large_bucket_count, small_bucket_count))
+        logger.error('Threshold of %3.3f was too large. Large Bucket Size: %i, Small Bucket Size: %i, Extra Bucket size: %i' 
+                     %(threshold, large_bucket_count, small_bucket_count, extra_bucket_count))
 
     # The threshold isn't too large.
     else:
-        logger.info('Threshold of %3.3f provides Large bucket size: %i, Small Bucket size: %i'
-                    %(threshold, large_bucket_count, small_bucket_count))
+        logger.info('Threshold of %3.3f provides Large bucket size: %i, Small Bucket size: %i, Extra Bucket size: %i'
+                    %(threshold, large_bucket_count, small_bucket_count, extra_bucket_count))
         x = df[df.columns[1:573]]
         y = df[df.columns[575]]
 
-        # SVM w/RBF kernel is our model.  We need to do this in conjuinction with Forward Selection and PCA
-        x, _ = fs_classifier(x, y)
-        x, _ = principal_component_analysis(x)
-        classifer_trainer(x, y)
+        # Create the feature set for the 3 classifiers.
+        rbf_params = {'gamma': [1e-2, 1e-3, 1e-4], 'C': [1, 10, 100, 1000]}
+        xgb_params = {'n_estimators': [50, 100, 150], 'max_depth': [1, 2, 3, 4]}
+        rf_params = {'class_weight': ['balanced'], 'n_estimators': [50, 100, 150], 'max_depth': [1, 2, 3, 4], 
+                       'min_samples_leaf': [1, 2, 3], 'min_samples_split': [1, 2, 3, 4]}
+
+        # Classifier pipeline for all 3 classifiers.
+        logger.info('\nSVC w/RBF Kernel Results:')
+        classifier_pipeline(x, y, SVC(kernel='rbf'), rbf_params)
+        logger.info('\nXGBoost Classifier Results:')
+        classifier_pipeline(x, y, XGBClassifier(), xgb_params)
+        logger.info('\nRandom Forest Classifier Results:')
+        classifier_pipeline(x, y, RandomForestClassifier(), rf_params)
 
     # Formatting for the logger.
     logger.info('-----------------------------------------------------')
@@ -418,7 +499,7 @@ def classifier():
     x = df[df.columns[1:573]]
     y = df[df.columns[575]]
 
-    x, sfs = fs_classifier(x, y)
+    x, sfs = fs_classifier(x, y, model=SVC(kernel='rbf'))
     x, pca = principal_component_analysis(x)
 
     # Fit the new x and y to a SVC w/rbf kernel.
@@ -457,12 +538,13 @@ def ki_pipeline(df=pd.DataFrame(), base_range=(-10,10)):
     reg = SVR(kernel='rbf')
     reg.fit(x_train, y_log_train)
     y_log_pred = reg.predict(x_valid)
-    y_pred = unscale(y_log_pred, base_range)
 
     # Reconvert back to base
+    y_pred = unscale(y_log_pred, base_range)
 
     # Calculate the rmse for both log and base.
     rmse_log = np.sqrt(mean_squared_error(y_log_valid, y_log_pred))
+    rmse = np.sqrt(mean_squared_error(y_valid, y_pred))
 
 
 # Calculating the MCC scores of the classifiers.
@@ -528,5 +610,3 @@ elif regression == True:
 ## Requirements.txt 
 ## python -m pip freeze
 ## pipe it into a txt file
-
-## Thresholds to test: 0.01, 0.1, 0.5, 2, 3, 5, 7, 10, 15, 17, 18, 20
