4993544c0f832291c6f48007cb0ab5c6e9186765
peter-splitice <pvpham92@gmail.com>
2022-09-05 22:23:00 -0700

    update git add script

---
+++ add_git.sh
@@ -0,0 +1,14 @@
+git add "KNN Classifier"
+git add "Random Forest Classifier"
+git add "SVC with RBF Kernel"
+git add "XGBoost Classifier"
+git add bucket_classifier.py
+git add HP_Test.log
+git add local_activate.sh
+git add testbench.ipynb
+git add "Threshold 0.01.log"
+git add "Threshold 0.10.log"
+git add "Threshold 0.50.log"
+git add "Threshold 5.00.log"
+git add "Threshold 10.00.log"
+git add "Threshold 15.00.log"
